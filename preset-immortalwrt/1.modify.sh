96665c5ad9fed0c0ce9c563e026a5e3600e3b903
Xf012138 <129813463+Xf012138@users.noreply.github.com>
2026-08-10 02:28:35 +0800

    Update 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -4,12 +4,14 @@
 #仅下载rkp-ipid，UA2F后续路由器装好系统再装
 git clone --depth 1 https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
 
-# ==========只保留CR6608，屏蔽全部mt7621设备定义==========
-#1、注释所有设备块
+#1.注释全部设备定义区块
 sed -i '/define Device\//,/endef/s/^/#/' target/linux/ramips/image/mt7621.mk
-#2、解开小米cr6608注释，启用你唯一需要的机型
+#2.注释全部设备加载语句（最关键，清空待编译设备清单）
+sed -i '/TARGET_DEVICES +=/s/^/#/' target/linux/ramips/image/mt7621.mk
+
+#3.解开小米cr6608注释，启用你唯一需要的机型
 sed -i '/define Device\/xiaomi_mi-router-cr6608/,/endef/s/^#//' target/linux/ramips/image/mt7621.mk
 sed -i '/TARGET_DEVICES += xiaomi_mi-router-cr6608/s/^#//' target/linux/ramips/image/mt7621.mk
 
-# 修复argon主题 wget‑any 依赖警告
+# 修复argon主题 wget‑any 依赖告警
 sed -i 's/wget-any/wget/g' package/luci-theme-argon/Makefile
