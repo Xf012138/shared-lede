defb8dc69acec4b813b9e8809c147b833873d071
Xf012138 <129813463+Xf012138@users.noreply.github.com>
2026-08-09 19:39:06 +0800

    Update 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-
 #修改登录IP
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # ==============================================
 # 第三方插件源码
 # ==============================================
 
-git clone --depth 1 https://github.com/Zxilly/UA2F.git package/UA2F
+# 完整克隆（不能使用depth 1，否则无法切换旧提交）
+git clone https://github.com/Zxilly/UA2F.git package/UA2F
 cd package/UA2F
 git checkout 7d42096
 cd ../..
