0eeecd8c974039cecac6507d4f65f796fdbe2922
Xf012138 <129813463+Xf012138@users.noreply.github.com>
2026-08-10 00:42:12 +0800

    Update 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -3,3 +3,5 @@
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 #仅下载rkp‑ipid，UA2F后续路由器装好系统再装
 git clone --depth 1 https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
+# 删除小米3G‑V2设备定义，阻止它编译缺失的dts
+sed -i '/xiaomi_mi-router-3g-v2/d' target/linux/ramips/image/mt7621.mk
