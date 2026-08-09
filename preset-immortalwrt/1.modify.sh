a88184511022de394f71fc8859dea10e50d3887c
Xf012138 <129813463+Xf012138@users.noreply.github.com>
2026-08-09 12:01:14 +0800

    Update 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -2,3 +2,12 @@
 
 #修改登录IP
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# ============================================
+# 第三方插件源码
+# ============================================
+
+# UA2F（UA伪装）
+git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
+
+# rkp-ipid（IP ID 随机化）
+git clone https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
