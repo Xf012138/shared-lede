6c87b185b71c137e637a33356c89ed1709afcc5e
Xf012138 <129813463+Xf012138@users.noreply.github.com>
2026-08-09 20:26:11 +0800

    Update 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 #修改登录IP
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-# ==============================================
-# 第三方插件源码
-# ==============================================
 
-# 完整克隆（不能使用depth 1，否则无法切换旧提交）
-git clone --depth 1 https://github.com/Zxilly/UA2F.git package/UA2F
-sed -i '/cmake_policy.*CMP0135/d' package/UA2F/CMakeLists.txt
+#仅下载 rkp‑ipid
 git clone --depth 1 https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
